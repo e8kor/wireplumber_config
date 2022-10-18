3e2dddb14f07313dde6f26ae2379a2cc970dad15
Eugene Korniichuk <eugenekorniichuk@pop-os.localdomain>
2022-10-18 18:37:45 +0200

    update config

--- bluetooth.lua.d/51-bluez-user-config.lua
+++ bluetooth.lua.d/51-bluez-user-config.lua
@@ -6,9 +6,30 @@ local rule_trekz = {
     {
       { "node.name", "equals", "bluez_output.20_74_CF_34_ED_F7.headset-head-unit" },
     },
+    {
+      { "node.name", "equals", "bluez_input.20_74_CF_34_ED_F7.headset-head-unit" },
+    }
   },
   apply_properties = {
     ["node.nick"] = "Trekz Headset",
+    ["media.target.tag"] = "work-hub;comm-hub;media-hub",
+  },
+}
+
+local rule_apple_air_pro = {
+  matches = {
+    {
+      { "node.name", "equals", "bluez_output.2C_76_00_CC_20_01.a2dp-sink" },
+    },
+    {
+      { "node.name", "equals", "bluez_output.2C_76_00_CC_20_01.headset-head-unit" },
+    },
+    {
+      { "node.name", "equals", "bluez_input.2C_76_00_CC_20_01.headset-head-unit" },
+    },
+  },
+  apply_properties = {
+    ["node.nick"] = "Apple AirPods Pro",
     ["media.target.tag"] = "work-hub;comm-hub;media-hub",
   },
 }
@@ -18,6 +39,12 @@ local rule_philips = {
     {
       { "node.name", "equals", "bluez_output.58_17_C1_07_48_52.a2dp-sink" },
     },
+    {
+      { "node.name", "equals", "bluez_output.58_17_C1_07_48_52.headset-head-unit" },
+    },
+    {
+      { "node.name", "equals", "bluez_input.58_17_C1_07_48_52.headset-head-unit" },
+    }
   },
   apply_properties = {
     ["node.nick"] = "Philips Headset",
@@ -30,6 +57,12 @@ local rule_bose = {
     {
       { "node.name", "equals", "bluez_output.4C_87_5D_28_F6_91.a2dp-sink" },
     },
+    {
+      { "node.name", "equals", "bluez_output.4C_87_5D_28_F6_91.headset-head-unit" },
+    },
+    {
+      { "node.name", "equals", "bluez_input.4C_87_5D_28_F6_91.headset-head-unit" },
+    }
   },
   apply_properties = {
     ["node.nick"] = "Bose Headset",
@@ -48,6 +81,7 @@ local rule_disable = {
   },
 }
 
+table.insert(bluez_monitor.rules, rule_apple_air_pro)
 table.insert(bluez_monitor.rules, rule_philips)
 table.insert(bluez_monitor.rules, rule_trekz)
 table.insert(bluez_monitor.rules, rule_bose)
